e5a934257899445f0d28fc8b56e862941d1e077d
FerdiHS <ferdi.fhs.fhs@gmail.com>
2024-05-28 22:57:28 +0800

    Fix bug in login using username

--- src/components/authentication_modal/LoginModal.tsx
+++ src/components/authentication_modal/LoginModal.tsx
@@ -71,7 +71,7 @@ const LoginModal = ({cancel, showSignUpModal, showChangePasswordModal} : {cancel
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                username: emailOrUsername.toLowerCase(),
+                username: emailOrUsername,
                 password: password,
             }),
         }).then(response => response.json())
